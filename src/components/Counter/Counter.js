f9be51ab639f74af92d107af5585079f89c52a47
moshiur25524 <moshiur.rahman25524@gmail.com>
2022-12-22 18:48:37 +0600

    make a counter button using react-redux

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,13 +1,41 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { decrementCount, incrementCount, resetCount } from '../../Services/Actions/CounterAction';
 
 const Counter = () => {
 
+    const count = useSelector(state => state.count)
+    const dispatch = useDispatch()
+    const handleIncrese = () => {
+        dispatch(incrementCount())
+    }
+    const handleReset = () => {
+        dispatch(resetCount())
+    }
+    const handleDecrease = () => {
+        dispatch(decrementCount())
+    }
     return (
         <div>
-            <p>Counter: 0</p>
-            <button>Add Number</button>
+            <p>Counter: {count}</p>
+            <button onClick={handleIncrese}>Increase Number</button>
+            <button onClick={handleReset}>RESET Number</button>
+            <button onClick={handleDecrease}>Decrease Number</button>
         </div>
     );
 };
+
+/*
+ 1. Declare a state: {count: 0}
+ 2. action {increase , decrease, reset}
+ 3. reducer INCREASE: count => count + 1
+            DECREASE: count => count - 1
+            RESET: count => count = 0
+4. Create a store
+5. provide the store in the root with provider
+6. To access in any state we will use useSelector from react-redux
+7. import dispatch and call from the react-redux
+8. dispatch the action indivitually.
+*/
 
 export default Counter;
